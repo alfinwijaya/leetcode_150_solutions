849fb9ebf05a244c6d57e1e377c43aa9fe84d5b3
Alfin Wijaya <wijayaalfin@gmail.com>
2023-11-04 15:38:27 +0700

    26. Remove Duplicates from Sorted Array

---
+++ leetcode_150_solutions/26_Remove_Duplicates_from_Sorted_Array.cs
@@ -0,0 +1,34 @@
+﻿public static class _26_Remove_Duplicates_from_Sorted_Array
+{
+    public static int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length == 0) return 0;
+        else if (nums.Length == 1 || (nums.Length == 2 && nums[0] == nums[1])) return 1;
+
+        int inc = 1;
+        int max = nums[nums.Length - 1];
+
+        while (nums[inc] != max)
+        {
+            if (nums[inc] == nums[inc - 1])
+            {
+                int secondInc = inc;
+                while (nums[secondInc - 1] != max)
+                {
+                    int temp = nums[secondInc];
+                    nums[secondInc] = nums[secondInc + 1];
+                    nums[secondInc + 1] = temp;
+                    secondInc++;
+                }
+            }
+            else
+            {
+                inc++;
+            }
+
+        }
+
+        return Array.IndexOf(nums, max) + 1;
+    }
+}
+
--- leetcode_150_solutions/Program.cs
+++ leetcode_150_solutions/Program.cs
@@ -6,9 +6,19 @@
 
     switch (input)
     {
+        case "26":
+            var nums = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            int result = _26_Remove_Duplicates_from_Sorted_Array.RemoveDuplicates(nums);
+
+            Console.WriteLine(result);
+            nums.ToList().ForEach(x => Console.Write($"{x} "));
+
+            Console.WriteLine();
+            break;
+
         case "27":
-            var nums = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }; var val = 2;
-            int result = _27_RemoveElement.RemoveElement(nums, val);
+            nums = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }; var val = 2;
+            result = _27_RemoveElement.RemoveElement(nums, val);
 
             Console.WriteLine(result);
             nums.ToList().ForEach(x => Console.Write($"{x} "));
@@ -21,6 +31,7 @@
             Console.WriteLine(result);
             nums.ToList().ForEach(x => Console.Write($"{x} "));
             break;
+
         case "88":
 
             var nums1 = new int[] { 1, 2, 3, 0, 0, 0 }; var m = 3; var nums2 = new int[] { 2, 5, 6 }; var n = 3;

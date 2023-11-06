5ea3a180dc045287b8cbe37e792426e84063ec6d
Alfin <alfin.wijaya@agungsedayu.com>
2023-11-06 16:02:38 +0700

    121. Best Time to Buy and Sell Stock 169. Majority Element

---
+++ leetcode_150_solutions/121_Best_Time_to_Buy_and_Sell_Stock.cs
@@ -0,0 +1,21 @@
+﻿public static class _121_Best_Time_to_Buy_and_Sell_Stock
+{
+    public static int MaxProfit(int[] prices)
+    {
+        int cheapestBuy = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.Length; i++)
+        {
+            if (prices[i] < cheapestBuy)
+            {
+                cheapestBuy = prices[i];
+            }
+
+            if (prices[i] - cheapestBuy > maxProfit)
+            {
+                maxProfit = prices[i] - cheapestBuy;
+            }
+        }
+        return maxProfit;
+    }
+}
--- leetcode_150_solutions/Program.cs
+++ leetcode_150_solutions/Program.cs
@@ -55,7 +55,36 @@
             nums1 = new int[] { 2, 0 }; m = 1; nums2 = new int[] { 1 }; n = 1;
             _88_MergeSortedArray.Merge(nums1, m, nums2, n);
             nums1.ToList().ForEach(x => Console.Write($"{x} "));
+            break;
 
+        case "121":
+            nums = new int[] { 7, 1, 5, 3, 6, 4 }; 
+            result = _121_Best_Time_to_Buy_and_Sell_Stock.MaxProfit(nums);
+
+            Console.WriteLine(result);
+            nums.ToList().ForEach(x => Console.Write($"{x} "));
+
+            Console.WriteLine();
+
+            nums = new int[] { 7, 6, 4, 3, 1 };
+            result = _121_Best_Time_to_Buy_and_Sell_Stock.MaxProfit(nums);
+
+            Console.WriteLine(result);
+            nums.ToList().ForEach(x => Console.Write($"{x} "));
+            break;
+
+        case "169":
+            nums = new int[] { 2, 2, 1, 1, 1, 2, 2 };
+            result = _169_Majority_Element.MajorityElement(nums);
+
+            Console.WriteLine(result);
+
+            Console.WriteLine();
+
+            nums = new int[] { 4, 5, 4 };
+            result = _169_Majority_Element.MajorityElement(nums);
+
+            Console.WriteLine(result);
             break;
 
         default:

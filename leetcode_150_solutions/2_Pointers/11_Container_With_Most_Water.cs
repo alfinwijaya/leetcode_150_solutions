b0a565415db0128f294d44c0f19c6c6624845ecb
Alfin Wijaya <wijayaalfin@gmail.com>
2023-11-28 21:29:23 +0700

    11. Container With Most Water

---
+++ leetcode_150_solutions/2_Pointers/11_Container_With_Most_Water.cs
@@ -0,0 +1,27 @@
+﻿public static class _11_Container_With_Most_Water
+{
+    public static int MaxArea(int[] height)
+    {
+        int p1 = 0;
+        int p2 = height.Length - 1;
+        int max = 0;
+
+        while (p1 < p2)
+        {
+            int area = Math.Min(height[p1], height[p2]) * (p2 - p1);
+            if (area > max)
+            {
+                max = area;
+            }
+            else if (height[p2] > height[p1])
+            {
+                p1++;
+            }
+            else
+            {
+                p2--;
+            }
+        }
+        return max;
+    }
+}
